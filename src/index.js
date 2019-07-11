571a178db41f13a9297563b0f63fb524bf5f7452
Eugene <eugeneburlak@yandex.ru>
2019-07-11 17:35:37 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Modal from './Modal';
-class App extends Component {
-  render() {
-    return (
-      <Modal opened={true}>
-        ds
-      </Modal>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
 export default Modal;
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <Modal opened={true}>
+//         ds
+//       </Modal>
+//     );
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
